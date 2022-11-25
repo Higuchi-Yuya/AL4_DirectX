35258cc1fb1e07cfd8f25d559fadea41430c68e6
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-11-25 14:22:02 +0900

    球と平面の当たり判定をテキストで確認できた。

---
+++ Collision.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CollisionPrimitive.h"
+
+class Collision
+{
+public:
+
+	// ‹…‚Æ•½–Ê‚Ì“–‚½‚è”»’è
+	static bool CheckSphere2Plane(const Sphere& sphere, const Plane& plane, DirectX::XMVECTOR* inter = nullptr);
+};
+
